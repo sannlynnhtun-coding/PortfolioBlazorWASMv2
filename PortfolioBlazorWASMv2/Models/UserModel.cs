292d8199a9510653e2574beed5af9699579eed84
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-08-31 23:02:55 +0630

    Add project files.

---
+++ PortfolioBlazorWASMv2/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioBlazorWASMv2.Models
+{
+    public class UserModel
+    {
+        public string name { get; set; }
+        public string position { get; set; }
+        public string city { get; set; }
+        public string info { get; set; }
+    }
+}
